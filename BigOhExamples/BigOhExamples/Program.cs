f077ee0242e8cb49527b484c2207bee4720e39c3
micriteo <76212872+micriteo@users.noreply.github.com>
2023-02-04 11:43:45 +0100

    Quick Sort and LoopArrayExample added

--- BigOhExamples/BigOhExamples/Program.cs
+++ BigOhExamples/BigOhExamples/Program.cs
@@ -7,15 +7,41 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            //Binary Search Complexity O(log n)
             //The list that will be searched
             int[] arr = new int[] { 16, 27, 43, 45, 56, 67, 78, 89, 90, 100 };
             //The number we are searching for
             int target = 43;
             int result = BinarySearch(arr, target);
             Console.WriteLine("Index of " + target + " is " + result);
+
+
+            //Looping through an ArrayList 
+            //Complexity O(n)
+            Console.WriteLine("Enter a number");
+            int number = Int32.Parse(Console.ReadLine());
+            string numbersShown = LoopArrayList(number);
+            Console.WriteLine("The list of numbers created is  " + numbersShown);
+
+
+            //Quick Sort
+            //Complexity O(n log n)
+
+            //Providing an unsorted list 
+            int[] arr2 = { 23, 7, 8, 30, 1, 6 };
+            //Getting the length of the array;
+            int n = arr2.Length;
+
+
+            QuickSort(arr, 0, n - 1);
+
+            Console.WriteLine("Sorted array is: ");
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+
         }
-        
-        static int BinarySearch(int[]arr, int target)
+
+        public static int BinarySearch(int[] arr, int target)
         {
             //Setting the left margin
             int left = 0;
@@ -45,7 +71,56 @@ namespace BinarySearch
             }
             //If the value we are looking for has not been found then -1 is returned 
             return -1;
+        }
+
+        public static string LoopArrayList(int number)
+        {
+            string overview = "";
+            for (int i = 0; i < number; i++)
+            {
+                overview += i + " ";
+            }
+            return overview;
+        }
+
+
+        
+        static int Partition(int[] arr, int low, int high)
+        {
+            
+            //Taking
+            int pivot = arr[high];
+
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp1;
+
+            return i + 1;
+        }
+
+        static void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(arr, low, high);
+
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
             }
         }
     }
+}
     
