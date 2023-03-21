71a663c5a27aa71a4590775a05d6754076ee4dc3
micriteo <micriteo@yahoo.com>
2023-03-21 17:35:30 +0100

    Smalll Update

--- FinalAssignment/FinalAssignment/FinalAssignment/CustomArrayList.cs
+++ FinalAssignment/FinalAssignment/FinalAssignment/CustomArrayList.cs
@@ -31,9 +31,27 @@ public class CustomArrayList<T>:ISortable, ISearchable,IInsertable
         throw new NotImplementedException();
     }
 
+    public void Insert(T item)
+    {
+
+        if (item == null) throw new ArgumentNullException();
+
+        if(this._count == this._array.Length) 
+        {
+            T[] newArray = new T[_array.Length * 2];
+            Array.Copy(_array, 0, newArray, 0, _array.Length);
+            _array = newArray;
+        }
+
+        _array[_count] = item;
+        _count++;
+
+        // TODO: When array reaches capacity limit, increase the size of the array by the incrementer and keep the old values
+        throw new NotImplementedException();
+    }
+
     public void Insert(InputType obj)
     {
-        // TODO: When array reaches capacity limit, increase the size of the array by the incrementer and keep the old values
         throw new NotImplementedException();
     }
 }
